d5e8bb1f50b9d88783408675d5b71f97c5b491ea
Dieguidev <diegogaraycullas@gmail.com>
2024-08-05 15:35:09 -0500

    chore: Update ProjectForm component to use ProjectFormData

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,9 +1,10 @@
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import ProjectForm from "../../components/projects/ProjectForm";
+import { ProjectFormData } from "../../types";
 
 export default function CreateProjectView() {
-  const initialValues = {
+  const initialValues: ProjectFormData = {
     projectName: "",
     clientName: "",
     description: "",
@@ -15,7 +16,9 @@ export default function CreateProjectView() {
     formState: { errors },
   } = useForm({ defaultValues: initialValues });
 
-  const handleForm = (data) => {};
+  const handleForm = (data: ProjectFormData) => {
+    console.log(data);
+  };
 
   return (
     <>
@@ -39,7 +42,6 @@ export default function CreateProjectView() {
           onSubmit={handleSubmit(handleForm)}
           noValidate
         >
-
           <ProjectForm register={register} errors={errors} />
 
           <input
