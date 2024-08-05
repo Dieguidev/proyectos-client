88a934e0a80bdf7fc8e048b0befece4ea4950a73
Dieguidev <diegogaraycullas@gmail.com>
2024-08-05 18:45:04 -0500

    chore: Update API endpoint for creating projects

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -3,7 +3,7 @@ import { ProjectFormData } from "../types";
 
 export async function createProject(formData: ProjectFormData) {
   try {
-    const { data } = await api.post("/projects", formData);
+    const { data } = await api.post("/project", formData);
     console.log(data);
 
   } catch (error) {
