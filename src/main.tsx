a96a181f49d75755448df84838b13fe457f33e68
Dieguidev <diegogaraycullas@gmail.com>
2024-08-06 18:17:59 -0500

    feat: Update main.tsx to use React Query Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import './index.css'
-import Router from './router'
+import "./index.css";
+import Router from "./router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-
-    <Router/>
+      <Router />
+      <ReactQueryDevtools />
     </QueryClientProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
