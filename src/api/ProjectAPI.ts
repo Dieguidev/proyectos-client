394a8be0b8ac1dd17958ee876cdde74b65204a85
Dieguidev <diegogaraycullas@gmail.com>
2024-08-06 13:05:02 -0500

    feat: Update getProjects API function to use dashboardProjectSchema
    The getProjects API function in the ProjectAPI module is updated to use the newly added dashboardProjectSchema from the types module. This schema is used to parse and validate the response data from the server, ensuring that it matches the expected structure.

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,6 +1,7 @@
 import { isAxiosError } from "axios";
 import { api } from "../lib/axios";
-import { ProjectFormData } from "../types";
+import { dashboardProjectSchema, ProjectFormData } from "../types";
+
 
 export async function createProject(formData: ProjectFormData) {
   try {
@@ -19,9 +20,11 @@ export async function createProject(formData: ProjectFormData) {
 
 export async function getProjects() {
   try {
-    const {data} = await api.get("/project");
-    return data
-
+    const { data } = await api.get("/project");
+    const response = dashboardProjectSchema.safeParse(data);
+    if (response.success) {
+      return response.data
+    }
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       console.error(error.response.data.error);
--- src/types/index.ts
+++ src/types/index.ts
@@ -8,6 +8,20 @@ export const projectSchema = z.object({
   description: z.string(),
 });
 
+export const dashboardProjectSchema = z.object({
+  limit: z.number(),
+  next: z.string().nullable(),
+  page: z.number(),
+  prev: z.string().nullable(),
+  projects: z.array(projectSchema.pick({
+    id: true,
+    projectName: true,
+    clientName: true,
+    description: true
+  })),
+  total: z.number(),
+})
+
 export type Project = z.infer<typeof projectSchema>;
 
 export type ProjectFormData = Pick<Project, 'clientName' | 'projectName' | 'description'>
