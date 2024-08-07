8cfc70b7a6f1f72090b8e03e355e60fab6c9fa09
Dieguidev <diegogaraycullas@gmail.com>
2024-08-07 18:12:52 -0500

    feat: Update ProjectDetailView to fetch and display project data
    The ProjectDetailView component in the `src/views/projects/ProjectDetailView.tsx` file is updated to fetch and display project data using the `useQuery` hook from `@tanstack/react-query`. This allows the component to retrieve the project details from the server based on the `projectId` parameter in the URL. Additionally, error handling is implemented to redirect to a 404 page if there is an error fetching the data.

--- src/views/projects/ProjectDetailView.tsx
+++ src/views/projects/ProjectDetailView.tsx
@@ -1,7 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import { Navigate, useParams } from "react-router-dom";
+import { getProjectById } from "../../api/ProjectAPI";
+import EditProjectForm from "../../components/projects/EditProjectForm";
 
 
 export default function ProjectDetailView() {
-  return (
-    <div>ProjectDetailView</div>
+  const { projectId } = useParams();
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["editProject", projectId],
+    queryFn: () => getProjectById(projectId!),
+  });
+
+  if (isLoading) {
+    return "Cargando...";
+  }
+  if(isError) {
+    return <Navigate to="/404" />;
+  }
+
+  if(data) return (
+    <>
+      <h1 className="text-5xl font-black">{data.projectName}</h1>
+      <p className="text-2xl font-ligjt text-gray-500 mt-5">{data.description}</p>
+
+      <nav className="my-5 flex gap-3">
+        <button type="button" className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors">Agregar Tarea</button>
+      </nav>
+    </>
   )
 }
