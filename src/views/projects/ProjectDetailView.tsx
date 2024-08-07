98d260b06a09a836ea8d9654c8c35af15f165b18
Dieguidev <diegogaraycullas@gmail.com>
2024-08-07 18:18:03 -0500

    feat: Agregar modal para agregar tarea en ProjectDetailView

--- src/views/projects/ProjectDetailView.tsx
+++ src/views/projects/ProjectDetailView.tsx
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getProjectById } from "../../api/ProjectAPI";
-import EditProjectForm from "../../components/projects/EditProjectForm";
+import AddTaskModal from "../tasks/AddTaskModal";
 
 
 export default function ProjectDetailView() {
+
+  const navigate = useNavigate()
+
   const { projectId } = useParams();
 
   const { data, isLoading, isError } = useQuery({
@@ -15,18 +18,28 @@ export default function ProjectDetailView() {
   if (isLoading) {
     return "Cargando...";
   }
-  if(isError) {
+  if (isError) {
     return <Navigate to="/404" />;
   }
 
-  if(data) return (
-    <>
-      <h1 className="text-5xl font-black">{data.projectName}</h1>
-      <p className="text-2xl font-ligjt text-gray-500 mt-5">{data.description}</p>
+  if (data)
+    return (
+      <>
+        <h1 className="text-5xl font-black">{data.projectName}</h1>
+        <p className="text-2xl font-ligjt text-gray-500 mt-5">
+          {data.description}
+        </p>
 
-      <nav className="my-5 flex gap-3">
-        <button type="button" className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors">Agregar Tarea</button>
-      </nav>
-    </>
-  )
+        <nav className="my-5 flex gap-3">
+          <button
+            type="button"
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            onClick={() => navigate(`?newTask=true`)}
+          >
+            Agregar Tarea
+          </button>
+        </nav>
+        <AddTaskModal/>
+      </>
+    );
 }
