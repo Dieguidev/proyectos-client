19cfa7a1848b1fb47fcd4075a35648350686e072
Dieguidev <diegogaraycullas@gmail.com>
2024-08-17 09:50:00 -0500

    feat: Agregar función de confirmación de cuenta en AuthAPI.ts
    Se agrega la función "confirmAccount" en el archivo AuthAPI.ts para permitir a los usuarios confirmar su cuenta ingresando un código de confirmación. Esto mejora la experiencia del usuario al permitirles completar el proceso de confirmación de cuenta de manera segura y eficiente.

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,11 +1,22 @@
 import { isAxiosError } from "axios";
 import { api } from "../lib/axios";
-import { UserRegistrationForm } from "../types";
+import { ConfirmToken, UserRegistrationForm } from "../types";
 
 export async function createAccount(formData: UserRegistrationForm) {
   try {
     const { data } = await api.post("/auth/register", formData);
     return data.user
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function confirmAccount(formData: ConfirmToken) {
+  try {
+    const { data } = await api.post("/auth/confirm-account", formData);
+    return data
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);
