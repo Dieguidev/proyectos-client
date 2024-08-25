5942e5aa4e1e61bff46c561347157e295e13b50e
Dieguidev <diegogaraycullas@gmail.com>
2024-08-25 18:14:03 -0500

    feat: Mejorar funcionalidad de búsqueda de usuario en AddMemberForm y SearchResult

--- src/views/projects/ProjectTeamView.tsx
+++ src/views/projects/ProjectTeamView.tsx
@@ -1,7 +1,7 @@
 import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
 import AddMemberModal from "../../components/team/AddMemberModal";
-import { useQuery } from "@tanstack/react-query";
-import { getProjectTeam } from "../../api/TeamAPI";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getProjectTeam, removeUserFormProject } from "../../api/TeamAPI";
 import {
   Menu,
   MenuButton,
@@ -11,6 +11,7 @@ import {
 } from "@headlessui/react";
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
 import { Fragment } from "react";
+import { toast } from "react-toastify";
 
 export default function ProjectTeamView() {
   const navigate = useNavigate();
@@ -21,6 +22,16 @@ export default function ProjectTeamView() {
     queryKey: ["projectTeam", projectId],
     queryFn: () => getProjectTeam(projectId),
     retry: false,
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: removeUserFormProject,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      toast.success(data);
+    },
   });
 
   if (isLoading) return <p>Cargando...</p>;
@@ -58,10 +69,15 @@ export default function ProjectTeamView() {
             className="divide-y divide-gray-100 border border-gray-100 mt-10 bg-white shadow-lg"
           >
             {data?.map((member) => (
-              <li key={member.id} className="flex justify-between gap-x-6 px-5 py-10">
+              <li
+                key={member.id}
+                className="flex justify-between gap-x-6 px-5 py-10"
+              >
                 <div className="flex min-w-0 gap-x-4">
                   <div className="min-w-0 flex-auto space-y-2">
-                    <p className="text-2xl font-black text-gray-600">{member.name}</p>
+                    <p className="text-2xl font-black text-gray-600">
+                      {member.name}
+                    </p>
                     <p className="text-sm text-gray-400">{member.email}</p>
                   </div>
                 </div>
@@ -88,6 +104,9 @@ export default function ProjectTeamView() {
                           <button
                             type="button"
                             className="block px-3 py-1 text-sm leading-6 text-red-500"
+                            onClick={() => {
+                              mutate({ projectId, userId: member.id });
+                            }}
                           >
                             Eliminar del Proyecto
                           </button>
