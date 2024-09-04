fb180e098dd747878ed0b86803ac0bfee941a012
Dieguidev <diegogaraycullas@gmail.com>
2024-09-03 22:31:37 -0500

    feat: Detectando si el usuario creo la nota para que pueda eliminar

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+import { useAuth } from "../../hooks/useAuth";
 import { Note } from "../../types";
 import { formatDate } from "../../utils/utils";
 
@@ -6,6 +8,11 @@ type NoteDetailProps = {
 };
 
 export default function NoteDetail({ note }: NoteDetailProps) {
+  const { data, isLoading } = useAuth();
+  const canDelete = useMemo(() => data?.id === note.createdBy.id, [data]);
+
+  if (isLoading) return "Cargando...";
+
   return (
     <div className="p-3 flex justify-between items-center">
       <div>
@@ -15,6 +22,7 @@ export default function NoteDetail({ note }: NoteDetailProps) {
         </p>
         <p className="text-xs text-slate-500">{formatDate(note.createdAt)}</p>
       </div>
+      {canDelete && <button  type="button" className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors">Eliminar</button>}
     </div>
   );
 }
