d7d5f939ac7125e916537c48ac1415e00c1c710f
Dieguidev <diegogaraycullas@gmail.com>
2024-09-04 19:36:01 -0500

    feat: modelando ProfileForm.tsx

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -7,43 +7,41 @@ import { NavMenu } from "../components/NavMenu";
 import { useAuth } from "../hooks/useAuth";
 
 export default function AppLayout() {
+  const { data, isError, isLoading } = useAuth();
 
-  const {data,isError,isLoading} = useAuth()
-
-  if(isLoading){
-    return <p>Cargando...</p>
+  if (isLoading) {
+    return <p>Cargando...</p>;
   }
-  if(isError){
-    return <Navigate to={'auth/login'}/>
+  if (isError) {
+    return <Navigate to={"/auth/login"} />;
   }
 
+  if (data)
+    return (
+      <>
+        <header className="bg-gray-800 py-5">
+          <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+            <div className="w-64">
+              <Link to={"/"}>
+                <Logo />
+              </Link>
+            </div>
 
-
-  if(data) return (
-    <>
-      <header className="bg-gray-800 py-5">
-        <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
-          <div className="w-64">
-            <Link to={"/"}>
-              <Logo />
-            </Link>
+            <NavMenu name={data.name} />
           </div>
+        </header>
 
-          <NavMenu name={data.name}/>
-        </div>
-      </header>
+        <section className="max-w-screen-2xl mx-auto mt-10 p-5">
+          <Outlet />
+        </section>
 
-      <section className="max-w-screen-2xl mx-auto mt-10 p-5">
-        <Outlet />
-      </section>
+        <footer className="p-5">
+          <p className="text-center">
+            Todos los derechos reservados &copy;{new Date().getFullYear()}
+          </p>
+        </footer>
 
-      <footer className="p-5">
-        <p className="text-center">
-          Todos los derechos reservados &copy;{new Date().getFullYear()}
-        </p>
-      </footer>
-
-      <ToastContainer pauseOnHover={false} pauseOnFocusLoss={false} />
-    </>
-  );
+        <ToastContainer pauseOnHover={false} pauseOnFocusLoss={false} />
+      </>
+    );
 }
