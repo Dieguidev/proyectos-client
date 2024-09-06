39102a753c6eceb16464e5627c82b802c5523c2b
Dieguidev <diegogaraycullas@gmail.com>
2024-09-05 21:11:50 -0500

    feat: Agregar funcionalidad de arrastrar y soltar tareas en TaskCard y TaskList
    Se agregó la funcionalidad de arrastrar y soltar tareas en los componentes TaskCard y TaskList. Esto permite a los usuarios reorganizar las tareas arrastrándolas y soltándolas en diferentes estados. Además, se agregó la dependencia @dnd-kit/core para habilitar esta funcionalidad.
    
    Los cambios específicos incluyen:
    - En TaskCard.tsx, se agregó una nueva propiedad de estilo para transformar la posición de la tarea en función de su posición de arrastre.
    - En TaskList.tsx, se importó la dependencia DndContext de @dnd-kit/core y se envolvieron los componentes relacionados con la funcionalidad de arrastrar y soltar en un contexto de arrastre y suelta.
    - Se agregó el componente DropTask en TaskList.tsx para permitir que las tareas se puedan soltar en diferentes estados.
    - Se realizaron ajustes en el diseño y la estructura de los componentes para acomodar la nueva funcionalidad.
    
    Estos cambios mejoran la experiencia del usuario al proporcionar una forma intuitiva de gestionar las tareas en la aplicación.

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,3 +1,4 @@
+import { DndContext } from "@dnd-kit/core";
 import { statusTranslation } from "../../locales/es";
 import { Task } from "../../types";
 import DropTask from "./DropTask";
@@ -20,15 +21,12 @@ const initialStatusGroups: GroupedTasks = {
   completed: [],
 };
 
-
-
 const statusStyle: { [key: string]: string } = {
   pending: "border-t-slate-500",
   onHold: "border-t-red-500",
   inProgress: "border-t-blue-500",
   underReview: "border-t-amber-500",
   completed: "border-t-emerald-500",
-
 };
 
 export default function TaskList({ tasks, canEdit }: TaskListProps) {
@@ -43,27 +41,31 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
       <h2 className="text-5xl font-black my-10">Tareas</h2>
 
       <div className="flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32">
-        {Object.entries(groupedTasks).map(([status, tasks]) => (
-          <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
-            <h3
-              className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyle[status]}`}
-            >
-              {statusTranslation[status]}
-            </h3>
+        <DndContext>
+          {Object.entries(groupedTasks).map(([status, tasks]) => (
+            <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
+              <h3
+                className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyle[status]}`}
+              >
+                {statusTranslation[status]}
+              </h3>
 
-            <DropTask/>
+              <DropTask />
 
-            <ul className="mt-5 space-y-5">
-              {tasks.length === 0 ? (
-                <li className="text-gray-500 text-center pt-3">
-                  No Hay tareas
-                </li>
-              ) : (
-                tasks.map((task) => <TaskCard key={task.id} task={task} canEdit={canEdit}/>)
-              )}
-            </ul>
-          </div>
-        ))}
+              <ul className="mt-5 space-y-5">
+                {tasks.length === 0 ? (
+                  <li className="text-gray-500 text-center pt-3">
+                    No Hay tareas
+                  </li>
+                ) : (
+                  tasks.map((task) => (
+                    <TaskCard key={task.id} task={task} canEdit={canEdit} />
+                  ))
+                )}
+              </ul>
+            </div>
+          ))}
+        </DndContext>
       </div>
     </>
   );
