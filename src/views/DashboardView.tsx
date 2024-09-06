d88b83d5739ffb2e7c53aba99249c89f019e3d59
Dieguidev <diegogaraycullas@gmail.com>
2024-09-05 19:03:40 -0500

    feat: Creando un Modal para que el usuario coloque su password si desea eliminar en el DashboardView.tsx
    Se agregó soporte para eliminar proyectos en el DashboardView.tsx. Se implementó una función y una mutación para eliminar un proyecto específico. Además, se agregó un modal de eliminación de proyecto en el componente DeleteProjectModal.tsx. Ahora, los usuarios pueden eliminar proyectos desde la vista del panel de control.

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -8,14 +8,17 @@ import {
 } from "@headlessui/react";
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
 
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { deleteProject, getProjects } from "../api/ProjectAPI";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 import { useAuth } from "../hooks/useAuth";
 import { isManager } from "../utils/policies";
+import DeleteProjectModal from "../components/projects/DeleteProjectModal";
 
 export default function DashboardView() {
+  const location = useLocation();
+  const navigate = useNavigate();
   const { data: user, isLoading: authLoading } = useAuth();
 
   const { data, isLoading } = useQuery({
@@ -137,7 +140,7 @@ export default function DashboardView() {
                                 <button
                                   type="button"
                                   className="block px-3 py-1 text-sm leading-6 text-red-500"
-                                  onClick={() => mutate(project.id)}
+                                  onClick={() => navigate(location.pathname + `?deleteProject=${project.id}`)}
                                 >
                                   Eliminar Proyecto
                                 </button>
@@ -162,10 +165,8 @@ export default function DashboardView() {
               </Link>
             </p>
           )
-          // data.projects.map((project) => (
-          //   <p>{project.clientName}</p>
-          // ))
         }
+        <DeleteProjectModal/>
       </>
     );
 }
